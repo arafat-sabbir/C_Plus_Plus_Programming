76a2d8950687fac62223cfc8f0d6ea468b69ff96
arafat-sabbir <arafatshabbir8@gmail.com>
2024-05-23 02:15:00 +0600

    Explored Input And Typecasting In C++

--- First.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-int main()
-{
-    int x = 10;
-    char b = 'a';
-    std::cout << "my age is" << std::endl << x;
-    return 0;
-}
---
+++ Week1/input.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int a;
+    float b;
+    char c;
+    cin >> a >> b >> c;
+
+    cout << a << ' ' << b << ' ' << int(c) << ' ';
+    return 0;
+}

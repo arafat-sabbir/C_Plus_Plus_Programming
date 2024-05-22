22c3cc0483ecbc104753766aeb941473fee4ae6c
arafat-sabbir <arafatshabbir8@gmail.com>
2024-05-23 02:35:53 +0600

    Explored Condition on C++

---
+++ Week1/condition1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 10;
+    if (a > 5)
+    {
+        cout << "A is Greater Then 5\n";
+    }
+    else
+    {
+        cout << "A is smaller Then 5" << endl;
+    }
+    cout << "this is global";
+    return 0;
+}

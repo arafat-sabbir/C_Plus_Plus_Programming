448f1ea6cff0e01322c3b1e1dad729bdbe1848ab
arafat-sabbir <arafatshabbir8@gmail.com>
2024-05-26 16:21:00 +0600

    Started Week2 Module2 String

---
+++ Week2/Module_1/String.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  string m;
+  string k  = "arafatSabbir";
+  cin>>m;
+  if(m ==k){
+    cout<<"same"<<endl;
+  }else{
+    cout<<"Different"<<endl;
+  }
+  cout<<m;
+  return 0;
+}

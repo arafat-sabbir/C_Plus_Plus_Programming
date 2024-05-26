e044858fdaf940db5aa13e55bd1dfd7b1003ba04
arafat-sabbir <arafatshabbir8@gmail.com>
2024-05-26 16:33:30 +0600

    Explored Element Access Function in String in Cpp

---
+++ Week2/Module_1/Element_Access.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  string s;
+  cin>>s;
+//   s.at Gives the value of the specified Index
+  cout<<s.at(0);
+//   s.back gives the last index value
+  cout<<s.back();
+//   s.front gives the first index value
+  cout<<s.front();
+  return 0;
+}

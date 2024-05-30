35a8222052c9d86d37ebebba70be3c35333200bc
arafat-sabbir <arafatshabbir8@gmail.com>
2024-05-31 00:18:58 +0600

    Explored How to take String Input With Space In Cpp

---
+++ Week2/Module_1/String_Input_Space.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin>>x;
+    // use cin.ignore() to ignore the enter
+    cin.ignore();
+    // or use getchar
+    // getchar()
+  string c;
+  getline(cin,c);
+  cout<<x<<endl;
+  cout<<c;
+  return 0;
+}

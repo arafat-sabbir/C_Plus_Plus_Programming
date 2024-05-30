65e1dbd403acfbe392efcd384369d34fcc733d77
arafat-sabbir <arafatshabbir8@gmail.com>
2024-05-31 00:51:39 +0600

    Explored String Constructor in Cpp

---
+++ Week2/Module_2/String_Constructor.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+//   string s="Hello World";
+//   string s("Hello World");
+string k = "Hey There";
+string l("Hello There",6); //Hello
+cout<<l;
+string sm(4,'s'); //"ssss"
+cout<<sm;
+  string s(k,4); //There
+    cout<<s;
+  return 0;
+}
